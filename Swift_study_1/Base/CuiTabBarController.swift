8faab10d3a0818bfd6c9bcf9ced49b8687a97813
cuizhangqiang <1121948946@qq.com>
2018-08-23 17:58:44 +0800

    20180823

--- Swift_study_1/Base/CuiTabBarController.swift
+++ Swift_study_1/Base/CuiTabBarController.swift
@@ -21,11 +21,19 @@ class CuiTabBarController: UITabBarController {
         thirdNC.title="网络"
         self.viewControllers=[firstNC,secondNC,thirdNC]
         
+        let firstItem = UITabBarItem(title: "基础", image: UIImage(named: "tab_0"), selectedImage: UIImage(named: "tab_selected_0"))
+        let secondItem = UITabBarItem(title: "UI", image: UIImage(named: "tab_1"), selectedImage: UIImage(named: "tab_selected_1"))
+        let thirdItem = UITabBarItem(title: "网络", image: UIImage(named: "tab_2"), selectedImage: UIImage(named: "tab_selected_2"))
+        
+        firstNC.tabBarItem=firstItem
+        secondNC.tabBarItem=secondItem
+        thirdNC.tabBarItem=thirdItem
+        
         self.tabBar.isTranslucent=false //设置是否透明--这里的透明不是全透明，设置背景色会有模糊效果
         self.tabBar.barTintColor=UIColor.white //设置背景色
         self.tabBar.tintColor=mainColor //设置选中的颜色
         //self.tabBar.clipsToBounds=true //去掉系统的分割线
-    
+
     }
 
     override func didReceiveMemoryWarning() {
